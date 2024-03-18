334320b75ebcbc5eb10fb2e3fe9af693085857a4
ithar <ithar3333@gmail.com>
2024-03-18 21:26:31 +0100

    con

--- Frontend/src/Pages/lesprofile/ImageUpload.jsx
+++ Frontend/src/Pages/lesprofile/ImageUpload.jsx
@@ -1,24 +1,31 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import './imageuplod.css'
 
 function ImageUpload() {
     const [file, setFile] = useState(null);
     const [data, setData] = useState([]);
+    const [formData, setFormData] = useState({ name: '', description: '' });
+    const [profileData, setProfileData] = useState(null);
+    const [showModal, setShowModal] = useState(false);
     const fileInputRef = useRef(null);
 
     const handleFile = (event) => {
         setFile(event.target.files[0]);
     };
 
-    useEffect(() => {
-        axios.get('http://localhost:3001/')
-            .then(res => {
-                setData(res.data[0]);
-            })
-            .catch(err => console.log(err));
-    }, []);
+    const handleFormChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        setProfileData(formData);
+        setShowModal(false);
+    };
 
     const handleUpload = async () => {
         if (!file) {
@@ -45,79 +52,79 @@ function ImageUpload() {
         fileInputRef.current.click();
     };
 
+    useEffect(() => {
+        axios.get('http://localhost:3001/')
+            .then(res => {
+                setData(res.data[0]);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
     return (
         <div className="inner-banner">
             <section className="w3l-breadcrumb">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-7 d-flex justify-content-center align-items-center"> {/* Displaying image in the center */}
-                        <div style={{ position: "relative", width: "300px", height: "300px", overflow: "hidden", borderRadius: "50%", display: "inline-block", border: "2px solid #8A2BE2" }}>
-    <img src={"http://localhost:3001/images/" + data.image} alt="" style={{ maxWidth: "100%", height: "auto", borderRadius: "50%" }} />
-</div>
-<FontAwesomeIcon icon={faEdit} onClick={handleClick} style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(550%, 550%)", cursor: "pointer", zIndex: "1", backgroundColor: "#70218f", borderRadius: "50%", fontSize: "20px" }} />
-
-
+                        <div className="col-lg-7 d-flex justify-content-center align-items-center">
+                            <div style={{ position: "relative", width: "300px", height: "300px", overflow: "hidden", borderRadius: "50%", display: "inline-block", border: "8px solid #70218f" }}>
+                                <img src={"http://localhost:3001/images/" + data.image} alt="" style={{ maxWidth: "100%", height: "auto", borderRadius: "50%" }} />
+                            </div>
+                            <FontAwesomeIcon icon={faPlus} onClick={handleClick} style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(450%, 450%)", cursor: "pointer", zIndex: "1", backgroundColor: "#70218f", borderRadius: "50%", fontSize: "26px" }} />
                         </div>
                         <div className="col-lg-7">
                             <div className="custom-block-icon-wrap">
-                                {/* Icon to trigger file input */}
-                                {/* Hidden file input */}
                                 <input type="file" ref={fileInputRef} style={{ display: "none" }} onChange={handleFile} />
-
                                 <button onClick={handleUpload} style={{ marginTop: "5px", marginBottom: "5px" }}>Envoyer</button>
-                                
-                                
                             </div>
-                            <div className="col-lg-5">
-                        <div className="custom-block-info">
-                            <div className="custom-block-top d-flex mb-1">
-                                <small className="me-4">
-                                    <a href="#">
-                                        <i className="bi-play"></i>
-                                        Play now
-                                    </a>
-                                </small>
-                                <small>
-                                    <i className="bi-clock-fill custom-icon"></i>
-                                    50 Minutes
-                                </small>
-                                <small className="ms-auto">Episode <span className="badge">15</span></small>
-                            </div>
-                            <h2 className="mb-2">Modern Vintage</h2>
-                            <p>What is Content Marketing? If you are wondering what content marketing is all about, this is the place to start.</p>
-                            <p>You are not allowed to redistribute this template ZIP file on any other template collection website. Please contact TemplateMo for more information.</p>
-                            <p>Pod Talk HTML CSS Template is made by Bootstrap v5.2.2 framework. You are allowed to modify and use this template for your business websites.</p>
-                            <div className="profile-block profile-detail-block d-flex flex-wrap align-items-center mt-5">
-                                <div className="d-flex mb-3 mb-lg-0 mb-md-0">
-                                    <img src="src/assets/images/c1.jpg" className="profile-block-image img-fluid" alt=""/>
-                                    <p>
-                                        itsbahahaouas
-                                        <img src="images/verified.png" className="verified-image img-fluid" alt=""/>
-                                        <strong>Création de vidéos</strong>
-                                    </p>
-                                </div>
-                                <ul className="social-icon ms-lg-auto ms-md-auto">
-                                    <li className="social-icon-item">
-                                        <a href="https://www.facebook.com/baha.haouas.9/" className="social-icon-link fa fa-facebook"></a>
-                                    </li>
-                                    <li className="social-icon-item">
-                                        <a href="https://www.instagram.com/itsbahahaouas/" className="social-icon-link fa fa-instagram"></a>
-                                    </li>
-                                    <li className="social-icon-item">
-                                        <a href="https://www.tiktok.com/@justbahahaouas?is_from_webapp=1&sender_device=pc" className="social-icon-link">
-                                            <img src="src/assets/images/tiktok-16.png" alt="TikTok" />
-                                        </a>
-                                    </li>
-                                </ul>
+                        </div>
+                        <div className="col-lg-5 d-flex justify-content-end align-items-end">
+                            <div className="additional-button-div" style={{ position: "absolute", bottom: "0", right: "0" }}>
+                                <button style={{ marginTop: "5px", marginBottom: "5px" }} onClick={() => setShowModal(true)}>Modifier le profile</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                </div>
-            </div>
-        </section>
+            </section>
+            {showModal && (
+                <div className="modale">
+                    <div className="modal-contente">
+                        <span className="close" onClick={() => setShowModal(false)}>&times;</span>
+                        <form onSubmit={handleFormSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="description">Description</label>
+                                <textarea id="description" name="description" value={formData.description} onChange={handleFormChange} />
+                            </div>
+                            <div className="form-group">
+                    <label htmlFor="bio">Biographie</label>
+                  <textarea id="bio" name="bio" value={formData.bio} onChange={handleFormChange} />
+                        </div>
+                        <div className="form-group">
+              <label htmlFor="genre">Genre</label>
+                      <div>
+        <label>
+            <input type="checkbox" name="genre" value="male" checked={formData.genre === 'male'} onChange={handleFormChange} />
+            Male
+        </label>
     </div>
-
+    <div>
+        <label>
+            <input type="checkbox" name="genre" value="female" checked={formData.genre === 'female'} onChange={handleFormChange} />
+            Female
+        </label>
+    </div>
+    <div>
+        <label>
+            <input type="checkbox" name="genre" value="other" checked={formData.genre === 'other'} onChange={handleFormChange} />
+            Other
+        </label>
+    </div>
+    </div>
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 }
 
